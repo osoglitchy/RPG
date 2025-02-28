4b0138fe37fe3c222bfce71363296a15aa2ce095
zKCacer65 <zkcacer65@hhsd.org>
2025-02-28 12:33:24 -0500

    done intro

--- TextBasedRPG.java
+++ TextBasedRPG.java
@@ -20,11 +20,37 @@ public class TextBasedRPG {
         System.out.println("Press ENTER to begin...");
         scan.nextLine();
         intro();
+        System.out.println();
+        System.out.println("Which orb do you pick?...");
+        System.out.println("A. Blue Orb");
+        System.out.println("B. Red Orb");
+        String choiceOrb = scan.nextLine();
+        if (choiceOrb.equalsIgnoreCase("a"))
+        {
+            blueOrb();
+        }
+        if (choiceOrb.equalsIgnoreCase("a"))
+        {
+            redOrb();
+        }
     }
 
     public static void intro()
     {
         typewriter("Our story begins here...");
+        typewriter("You are currently within the great big national Greens Park staring off at the orbs in front of you.");
+        typewriter("Looking around the world you can see the vast futurism of the world as people walk around with robotic body parts and flying cars are abundant in this grand place.");
+        System.out.println();
+        typewriter("The world you reside in is one of controlled chaos that keeps it in its order, held there by the powerful cat god and the opposer dog devil.");
+        typewriter("The cat god and the dog devil have always been at odds bringing both creation and destruction within your world.");
+        typewriter("The cat god has acted as the yang while the dog devil remained the yin.");
+        typewriter("And occasionally within their bouts across the vast endless universe, they gave power to champions of their race to aid in the never ending fight between the two of them.");
+        typewriter("But here today the selection of champions happened again after hundreds of years passed from past to today.");
+        typewriter("It seemed this was their final bout for who would truly overcome who, but a freak accident happened within the time space.");
+        typewriter("With selection of heroes for each of their sides, one of the cats had been bestowed with the power of both the cat god and the dog devil before them.");
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        typewriter("That cat is YOU, "+name+", now you must choose your destiny from either taking the side of the cat god with the blue orb, or the dog devil of the red.");
 
         //use typewriter( ) to print out the intro setup for your story.
 
@@ -34,6 +60,14 @@ public class TextBasedRPG {
         //4. give us a quest
         //5. it should end with a choice/decision
 
+    }
+
+    public static void blueOrb() {
+        //enter code here
+    }
+
+    public static void redOrb() {
+        //enter code here
     }
 
     public static void typewriter(String text) {
